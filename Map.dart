64f1a22fafcd4491a0412066de315e27dc1d6dc6
Jahanzaib Khokhar <jahanzaibjk9@gmail.com>
2022-03-13 17:40:31 +0500

    Today Task Completed

---
+++ Map.dart
@@ -0,0 +1,34 @@
+void main(){
+
+// Map <String,dynamic> data ={'Name': 'Jahanzaib', 'Roll Number': '2101'};
+// print(data);
+// print(data.keys);
+// print(data.values);
+
+
+// Map<String,dynamic> data ={
+//     "Name": 'Jahanzaib', 
+//     "Age": 21,
+//     "Roll Number":'1089669',
+//     "Marks":95,
+//     "GPA":3.1,
+// "Subjects":["English","Urdu","Math"],
+// "Fav_Player": 'Boom Boom'
+//     };
+//  List std =[(data['GPA']),"jahanzaib",(data['Roll Number'])];
+
+// data.putIfAbsent("Fav_Player", ()=> 'C Ronaldo');
+
+// print(data);
+// print("Length: ${data.length}");
+// print("Your GPA:  ${std[0]}");
+// print("Student Name:  ${std[1]}");
+// print("Roll Number: ${std[2]}");
+
+for(var num=0; num <=3; num++){
+
+    print(num*3);
+}
+
+
+}
